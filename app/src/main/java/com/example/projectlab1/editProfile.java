e98d9b3d1c083629405445c292348b0234dcb642
Your Name <a.alghazali98@hotmail.com>
2019-10-26 20:54:22 +0300

    first Commit

---
+++ app/src/main/java/com/example/projectlab1/editProfile.java
@@ -0,0 +1,17 @@
+package com.example.projectlab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class editProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+
+
+}
